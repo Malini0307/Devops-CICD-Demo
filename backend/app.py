e05d7fb8aa35a8d06b5accae57b26df14c38ac44
Bocha <2420970@cognizant.com>
2026-08-26 20:58:50 +0530

    added comment

--- backend/app.py
+++ backend/app.py
@@ -3,6 +3,7 @@ from fastapi.middleware.cors import CORSMiddleware
 from pydantic import BaseModel
 from registration import EmployeeRegistrationService
 
+# Create FastAPI app
 app = FastAPI()
 
 app.add_middleware(
